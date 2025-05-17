228917e3671ba29459243687f2fba5e6a6bb45ce
m.couderc <m.couderc@komet-is.fr>
2025-05-17 15:42:16 +0200

    feat: modified project to generic starter

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all()->toResourceCollection();
+    }
+
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name'  => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return new UserResource($user);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $validated = $request->validate([
+            'name'  => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
+
+        $user->update($validated);
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\ScriptController;
-use App\Http\Middleware\AuthenticateOnceWithBasicAuth;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->withoutMiddleware('auth:sanctum')->group(
@@ -14,10 +12,5 @@ Route::prefix('auth')->withoutMiddleware('auth:sanctum')->group(
 );
 
 Route::middleware("auth:sanctum")->group(function () {
-    Route::get('/orders', [OrderController::class, 'index']);
-    Route::post('/orders', [OrderController::class, 'store']);
-    Route::post('logout', [AuthController::class, 'logout']); // Only authenticated users
-    Route::prefix('scripts')->group(function () {
-        Route::post('/run', [ScriptController::class, 'runPythonScript']);
-    });
+    Route::apiResource('users', UserController::class);
 });
